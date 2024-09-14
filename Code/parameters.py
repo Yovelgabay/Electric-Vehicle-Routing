245275dc8ce2767d4d6e9244028e034fb8e5fc13
Tal <talgaon4@gmail.com>
2024-09-14 22:42:04 +0300

    final adjustments

--- Code/parameters.py
+++ Code/parameters.py
@@ -5,7 +5,7 @@ NUM_ROUTE_POINTS = 30
 POPULATION_SIZE = 500
 GENERATIONS = 100
 MUTATION_RATE = 1
-NUM_POINTS = 200
+NUM_POINTS = 150
 MAX_STAGNATION = 20
 
 AVERAGE_QUEUEING_TIME = 12
@@ -14,11 +14,13 @@ MAX_QUEUEING_TIME = 20
 
 
 CHECK_POINTS = [
-    np.array([5, 90]),
+    np.array([5, 85]),
     np.array([12, 30]),
     np.array([20, 5]),
     np.array([36, 80]),
     np.array([40, 90]),
+    np.array([50, 40]),
+    np.array([55, 20]),
     np.array([70, 10]),
     np.array([90, 50])
 ]
